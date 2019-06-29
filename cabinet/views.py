6df81cbacd2a98b81f92bc2ed4d144a1d9f31bb2
Oleh Smolynets <smolynets@gmail.com>
2019-06-29 14:07:35 +0300

    added main_view and empty landing_page template

--- cabinet/views.py
+++ cabinet/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main_view(request):
+    if not request.user.is_authenticated:
+        return render(request, 'landing_page.html', {})
+    return render(request, 'home.html', {})
--- webschool/urls.py
+++ webschool/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from cabinet.views import main_view
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', main_view, name='main'),
 ]

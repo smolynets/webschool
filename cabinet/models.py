7c863af8f6ce2a0b788c4c579f00f3b1b66b710a
Oleh Smolynets <smolynets@gmail.com>
2019-06-29 14:48:48 +0300

    added Video model and fixed main_view

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    text = models.TextField(max_length=200)
+    video_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Video
+
+
 def main_view(request):
+    videos = Video.objects.all()
     if not request.user.is_authenticated:
         return render(request, 'landing_page.html', {})
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {"videos": videos})
